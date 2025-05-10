b136355b56019f7d743ba77e40f2bf9892695c31
Daniel Chanci <daniel.chanci@kinesso.com>
2025-05-10 10:06:29 -0500

    Correciones

--- Clase-4/inSiana/index.js
+++ Clase-4/inSiana/index.js
@@ -2,31 +2,46 @@ document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("porcentajeInput");
     const button = document.getElementById("mostrarBtn");
     const resultado = document.getElementById("resultado");
+    const spinner = document.getElementById("spinner");
   
     button.addEventListener("click", () => {
-      const valor = Number(input.value);
-      if (isNaN(valor) || valor < 0 || valor > 100) {
-        resultado.innerHTML = `<p class="resultado__error">Por favor, ingresa un número entre 0 y 100.</p>`;
-        return;
-      }
+        spinner.style.display = "flex";
+        const valor = Number(input.value);
+        if (isNaN(valor) || valor < 0 || valor > 100) {
+            resultado.innerHTML = `<p class="resultado__error">Por favor, ingresa un número entre 0 y 100.</p>`;
+            return;
+        }
 
-      let redondeado;
+        const delay = Math.random() * 6;
 
-      if (valor <= 5) {
-       redondeado = 5;
-      } else {
-        redondeado = Math.floor(valor / 5) * 5;
-      }
-  
-      
-      const imagenSrc = `img/${redondeado}.jpg`;
-  
-      resultado.innerHTML = `
-        <div class="resultado__card">
-          <h2 class="resultado__title">Avance: ${valor}%</h2>
-          <img src="${imagenSrc}" alt="Avance ${redondeado}%" class="resultado__image">
-        </div>
-      `;
+        setTimeout(() => {
+            spinner.style.display = "none";
+            if (delay >= 5) {
+              resultado.innerHTML = `<p class="resultado__error">Error: El tiempo de carga superó los 5 segundos.</p>`;
+            } else {
+              resultado.innerHTML = obtenerImagen(valor);
+            }
+          }, delay * 1000);
     });
-  });
+
+    function obtenerImagen(valor){
+        let redondeado;
+
+        if (valor <= 5) {
+            redondeado = 5;
+        } else {
+            redondeado = Math.floor(valor / 5) * 5;
+        }
+    
+        
+        const imagenSrc = `img/${redondeado}.jpg`;
+    
+        return `
+            <div class="resultado__card">
+            <h2 class="resultado__title">Avance: ${valor}%</h2>
+            <img src="${imagenSrc}" alt="Avance ${redondeado}%" class="resultado__image">
+            </div>
+        `;
+    }
+});
   
