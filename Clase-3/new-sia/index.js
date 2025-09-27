249f6a42e526787ec18fa6d75273885054685e40
Daniel Chanci <daniel.chanci@kinesso.com>
2025-09-27 09:30:13 -0500

    Clase-4-1

--- Clase-3/new-sia/index.js
+++ Clase-3/new-sia/index.js
@@ -1,260 +1,350 @@
-console.log('Hola a todos');
+// console.log('Hola a todos');
 
-if (true) {
-  var casa = 'muy linda';
-  let cel = 2;
-  const bool = true;
-}
+// if (true) {
+//   var casa = 'muy linda';
+//   let cel = 2;
+//   const bool = true;
+// }
 
-console.log(casa);
+// console.log(casa);
 
-// HOISTING
+// // HOISTING
 
-// string
-let Harrison = '     Pana ';
+// // string
+// let Harrison = '     Pana ';
 
-console.log(Harrison[3]);
-console.log(Harrison.length);
-console.log(Harrison.trim());
-console.log(Harrison.slice(4, 9));
+// console.log(Harrison[3]);
+// console.log(Harrison.length);
+// console.log(Harrison.trim());
+// console.log(Harrison.slice(4, 9));
 
-console.log(Harrison);
+// console.log(Harrison);
 
-// template literals
+// // template literals
 
-let nombre = 'Harrison';
-let resto = 'A mimir';
+// let nombre = 'Harrison';
+// let resto = 'A mimir';
 
-let palabra = nombre + resto;
-console.log(palabra);
+// let palabra = nombre + resto;
+// console.log(palabra);
 
-console.log(nombre + ' no te adelantes, por favor.' + resto);
-console.log(`${nombre} no te adelantes ${resto}`);
+// console.log(nombre + ' no te adelantes, por favor.' + resto);
+// console.log(`${nombre} no te adelantes ${resto}`);
 
 
-// JS IS WEIRD
+// // JS IS WEIRD
 
-console.log(2 + 2);
-console.log(5 - 2);
-console.log('2' + 5);
-console.log('2' - 3);
-console.log(nombre + '5' - 3);
-console.log(nombre + +nombre);
+// console.log(2 + 2);
+// console.log(5 - 2);
+// console.log('2' + 5);
+// console.log('2' - 3);
+// console.log(nombre + '5' - 3);
+// console.log(nombre + +nombre);
 
-if (nombre.length >= 0) {
-  console.log('a bueno')
-} else {
-  console.log('egvwsfgw');
-}
+// if (nombre.length >= 0) {
+//   console.log('a bueno')
+// } else {
+//   console.log('egvwsfgw');
+// }
 
-const sebas = nombre.length >= 50 ? 
-              'Un crack' : 
-              'Un cracnt';
+// const sebas = nombre.length >= 50 ? 
+//               'Un crack' : 
+//               'Un cracnt';
 
-console.log(sebas);
+// console.log(sebas);
 
-//falsy values
-//false
-//0
-//''
-//null
-//undefinded
-//NaN
+// //falsy values
+// //false
+// //0
+// //''
+// //null
+// //undefinded
+// //NaN
 
 
-const arreglo = [1, 2, 3, 4, 5, 6, 7];
-console.log(arreglo[4]);
-console.log(arreglo.length);
-console.log(arreglo[80]);
-arreglo[80] = 'rvev3ecv';
-console.log(arreglo);
+// const arreglo = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arreglo[4]);
+// console.log(arreglo.length);
+// console.log(arreglo[80]);
+// arreglo[80] = 'rvev3ecv';
+// console.log(arreglo);
 
-//métodos (No mutables)
+// //métodos (No mutables)
 
-const arr = arreglo.concat([1, 2 ,3]);
-console.log(arreglo, arr);
+// const arr = arreglo.concat([1, 2 ,3]);
+// console.log(arreglo, arr);
 
 
-console.log(arr.slice(0, 4));
-console.log(arr);
+// console.log(arr.slice(0, 4));
+// console.log(arr);
 
-//includes
+// //includes
 
-console.log(arr.includes(5, 7));
-console.log(arr.join(','));
-//métodos mutables
+// console.log(arr.includes(5, 7));
+// console.log(arr.join(','));
+// //métodos mutables
 
-arr.sort();
+// arr.sort();
 
-arr.push(20);
-console.log(arr);
+// arr.push(20);
+// console.log(arr);
 
-arr.pop();
-console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 
-console.log(arr.splice(0, 4));
-console.log(arr);
+// console.log(arr.splice(0, 4));
+// console.log(arr);
 
-//Objetos
+// //Objetos
 
-const person = {
-  nombre: 'David',
-  apellido: 'Villa'
-}
+// const person = {
+//   nombre: 'David',
+//   apellido: 'Villa'
+// }
 
-console.log(person);
+// console.log(person);
 
-//Enumerables vs iterables
+// //Enumerables vs iterables
 
-for (property in person) {
-  console.log(property, person[property]);
-}
+// for (property in person) {
+//   console.log(property, person[property]);
+// }
 
-for (property of Object.entries(person)) {
-  console.log(property);
-}
+// for (property of Object.entries(person)) {
+//   console.log(property);
+// }
 
-// REFERENCIA VS VALOR
+// // REFERENCIA VS VALOR
 
-let num = 5;
-num2 = num;
-num2 = 7;
-console.log(num, num2);
+// let num = 5;
+// num2 = num;
+// num2 = 7;
+// console.log(num, num2);
 
 
-person2 = person;
-person2.apellido = 'Chanci';
-console.log(person, person2);
+// person2 = person;
+// person2.apellido = 'Chanci';
+// console.log(person, person2);
 
-//funciones
+// //funciones
 
-saludar('Maria Alejandra');
+// saludar('Maria Alejandra');
 
-function saludar(nombre) {
-  console.log(`Hola ${nombre}`);
-}
+// function saludar(nombre) {
+//   console.log(`Hola ${nombre}`);
+// }
 
-const suma = function (a, b) {
-  return a + b;
-}
+// const suma = function (a, b) {
+//   return a + b;
+// }
 
-console.log(suma(2, 3));
+// console.log(suma(2, 3));
 
-console.log(saludar, suma);
+// console.log(saludar, suma);
 
-//this
+// //this
 
-console.log(this);
+// console.log(this);
 
-const object = {
-  name: 'Daniel Chanci',
-  sayMyName() {
-    return this.name
-  }
-}
+// const object = {
+//   name: 'Daniel Chanci',
+//   sayMyName() {
+//     return this.name
+//   }
+// }
 
-console.log(object.sayMyName());
+// console.log(object.sayMyName());
 
-function saludar2() {
-  console.log(`Holis soy ${this.name}`)
-}
+// function saludar2() {
+//   console.log(`Holis soy ${this.name}`)
+// }
 
-saludar2();
+// saludar2();
 
-const chanci = {
-  name: 'Juan',
-  greet: saludar2
-}
+// const chanci = {
+//   name: 'Juan',
+//   greet: saludar2
+// }
 
-chanci.greet()
+// chanci.greet()
 
 
-//Lexical scope
+// //Lexical scope
 
-function funcion1(){
-  let count = 0;
-  return function contador() {
-    count+= 1;
-    console.log(count)
-  }
-}
+// function funcion1(){
+//   let count = 0;
+//   return function contador() {
+//     count+= 1;
+//     console.log(count)
+//   }
+// }
 
-const miFunc = funcion1();
-miFunc();
-miFunc();
-miFunc();
-miFunc();
-miFunc();
+// const miFunc = funcion1();
+// miFunc();
+// miFunc();
+// miFunc();
+// miFunc();
+// miFunc();
 
-const harrisonFunc = funcion1()
-harrisonFunc();
-harrisonFunc();
+// const harrisonFunc = funcion1()
+// harrisonFunc();
+// harrisonFunc();
 
-//call, apply, bind
+// //call, apply, bind
 
-const sebas2 = {
-  name: 'Sebas',
-  age: 25,
-  greet: () => {
-    console.log(`Hola ${this.name}`)
-  }
-}
+// const sebas2 = {
+//   name: 'Sebas',
+//   age: 25,
+//   greet: () => {
+//     console.log(`Hola ${this.name}`)
+//   }
+// }
 
-const jp = {
-  name: 'Juan Pablo',
-  age: 20
-}
+// const jp = {
+//   name: 'Juan Pablo',
+//   age: 20
+// }
 
-sebas2.greet();
-sebas2.greet.call(jp);
+// sebas2.greet();
+// sebas2.greet.call(jp);
 
-//bind
+// //bind
 
-const newFunc = sebas2.greet.bind(jp);
-newFunc()
+// const newFunc = sebas2.greet.bind(jp);
+// newFunc()
 
-//callbacks
+// //callbacks
 
-function position(element, index) {
-  console.log(`pos=${index} caracter=${element}`)
-}
+// function position(element, index) {
+//   console.log(`pos=${index} caracter=${element}`)
+// }
 
-const array2 = [1,2,3,4,5,6,7];
+// const array2 = [1,2,3,4,5,6,7];
 
-array2.forEach(function (index, element) {
-  console.log(`pos=${index} caracter=${element}`)
+// array2.forEach(function (index, element) {
+//   console.log(`pos=${index} caracter=${element}`)
+// })
+
+// array2.forEach((element, index) => console.log(`pos=${index} caracter=${element}`));
+
+// array2.forEach(position);
+
+// //Arrow functions
+
+// const resta = function(a, b) {
+//   return a - b;
+// }
+
+// const reastArrFunc = (a, b) => a - b;
+
+// //prolog
+// const arr2 = []
+
+// for (let i = 0; i < array2.length; i++) {
+//   arr2[i] = array2[i] * 2;
+// }
+
+// //map
+
+// console.log(array2.map(num => num * 2));
+
+// //filter
+
+// console.log(array2.filter(num => num%2 === 0));
+
+// //some y every
+
+// console.log(array2.some(num => num%2 === 0));
+
+// console.log(array2.every(num => num%2 === 0));
+
+// //
+
+// const numeros = [1, 2, 3, 4, 5, 5,6,7];
+
+// //Devuelvan la suma de numeros
+
+// let suma2 = 0;
+
+// for (let i = 0; i < numeros.length; i++) {
+//   suma2 += numeros[i];
+// }
+
+// // reduce: Reduce un arreglo a un solo valor
+
+// const score = numeros.reduce((acc, curr) => acc + curr);
+
+// // reduce con objetos:
+
+// const frutas = ["manzana", "manzana", "pera", "uva", "uva", "uva"];
+
+// //Agrupa los elementos en un diccionario (objeto) y pon la cantidad por cada uno
+
+
+// const dicc = frutas.reduce((acc, fruta) => {
+//   // Conjunción: &&
+//   // Disyunción: ||
+//   // Nullish operator: ??
+//   // if (!acc[fruta]) {
+//   //   acc[fruta] = 0;
+//   // }
+//   // acc[fruta] += 1;
+//   acc[fruta] = (acc[fruta] ?? 0) + 1;
+//   return acc
+// } ,{});
+
+// console.log(dicc);
+
+// //ecma6
+
+// //spreadOperator
+
+// const cosasSaludables = [...frutas];
+
+// cosasSaludables.push('dormir');
+
+// console.log(cosasSaludables, frutas);
+
+// const otroObj = {...dicc, saludo: 'Hola'};
+
+// //Operador rest
+
+// function sum(...nums) {
+//   return nums.reduce((total, num) => total + num);
+// }
+
+// console.log(sum(1,2,3,4,5,6,7))
+
+// // Destructuración
+
+// const scores = [10,20,30,40,50,60,70];
+
+// //ordenar
+
+// scores.sort((a, b) => b - a);
+// console.log(scores);
+
+// const [gold, silver, bronze, ...left] = scores;
+// console.log(gold, silver, bronze, left);
+
+// // objetos
+
+// const {manzana: apple, pera} = dicc;
+// console.log(apple, pera);
+
+// // param destructuring
+
+// function llamar({manzana}) {
+//   console.log(`Tines ${manzana} manzanas`);
+// }
+
+// llamar(dicc);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('#porcentajeInput');
+  const button = document.querySelector('#mostrarBtn');
+  const resultado = document.querySelector('.resultado');
+
+  button.addEventListener('click', () => alert('Millosaurios'));
 })
-
-array2.forEach((element, index) => console.log(`pos=${index} caracter=${element}`));
-
-array2.forEach(position);
-
-//Arrow functions
-
-const resta = function(a, b) {
-  return a - b;
-}
-
-const reastArrFunc = (a, b) => a - b;
-
-//prolog
-const arr2 = []
-
-for (let i = 0; i < array2.length; i++) {
-  arr2[i] = array2[i] * 2;
-}
-
-//map
-
-console.log(array2.map(num => num * 2));
-
-//filter
-
-console.log(array2.filter(num => num%2 === 0));
-
-//some y every
-
-console.log(array2.some(num => num%2 === 0));
-
-console.log(array2.every(num => num%2 === 0));
